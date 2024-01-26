36ca394ee82a6bc7299ffa2d1cf137bf78ef6f5d
Eder Soares <edersoares@me.com>
2024-01-26 14:48:26 -0300

    Command expects a `type` and a `name`

--- src/Console/Commands/GenerateCommand.php
+++ src/Console/Commands/GenerateCommand.php
@@ -6,9 +6,12 @@ namespace Dex\Laravel\Studio\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\text;
+
 class GenerateCommand extends Command
 {
-    protected $signature = 'generate {arguments?*} {--preset=}';
+    protected $signature = 'generate {arguments?*}';
 
     protected $description = 'Generate new files';
 
@@ -16,6 +19,52 @@ class GenerateCommand extends Command
     {
         $this->comment('Generate new files');
 
+        [$type, $name] = $this->getTypeAndName();
+
         return self::SUCCESS;
+    }
+
+    private function preset(): string
+    {
+        $default = config('studio.preset');
+
+        return $preset ?? $default;
+    }
+
+    private function types(): array
+    {
+        $preset = $this->preset();
+        $config = config('studio.presets.' . $preset, []);
+
+        return array_keys($config);
+    }
+
+    private function isInvalidType(string $type): bool
+    {
+        return !$this->isValidType($type);
+    }
+
+    private function isValidType(string $type): bool
+    {
+        return in_array($type, $this->types(), true);
+    }
+
+    private function getTypeAndName(): array
+    {
+        $arguments = $this->argument('arguments');
+
+        $type = $arguments[0] ?? '';
+
+        if ($this->isInvalidType($type)) {
+            $type = select('What type of file would you generate?', $this->types());
+        }
+
+        $name = $arguments[1] ?? '';
+
+        if (empty($name)) {
+            $name = text('What name?', required: true);
+        }
+
+        return [$type, $name];
     }
 }
--- tests/Feature/Console/Commands/GenerateCommandTest.php
+++ tests/Feature/Console/Commands/GenerateCommandTest.php
@@ -6,3 +6,10 @@ test('`generate` command exists')
     ->artisan('generate model User')
     ->expectsOutput('Generate new files')
     ->assertOk();
+
+test('`generate` command expects a `type` and a `name`')
+    ->artisan('generate')
+    ->expectsChoice('What type of file would you generate?', 'model', ['model'])
+    ->expectsQuestion('What name?', 'User')
+    ->expectsOutput('Generate new files')
+    ->assertOk();
