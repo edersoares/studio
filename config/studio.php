24f6b413074bd25e286906d6b003beadb52dc5b1
Eder Soares <edersoares@me.com>
2024-01-29 13:13:51 -0300

    Presets

--- config/studio.php
+++ config/studio.php
@@ -2,6 +2,42 @@
 
 declare(strict_types=1);
 
+use function Orchestra\Testbench\package_path;
+use function Orchestra\Testbench\workbench_path;
+
 return [
-    // dex/studio
+
+    'preset' => env('STUDIO_PRESET', 'workbench'),
+
+    'presets' => [
+
+        'laravel' => [
+
+            'model' => [
+                'namespace' => 'App\\Models',
+                'path' => base_path('app/Models'),
+            ],
+
+        ],
+
+        'package' => [
+
+            'model' => [
+                'namespace' => 'Dex\\Laravel\\Studio\\Models',
+                'path' => package_path('app/Models'),
+            ],
+
+        ],
+
+        'workbench' => [
+
+            'model' => [
+                'namespace' => 'Workbench\\App\\Models',
+                'path' => workbench_path('app/Models'),
+            ],
+
+        ],
+
+    ],
+
 ];
