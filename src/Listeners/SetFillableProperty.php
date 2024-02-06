980498649d0ea1581dba8e12305b7b674c3c5d48
Eder Soares <edersoares@me.com>
2024-02-06 09:22:41 -0300

    Set fillable

---
+++ src/Listeners/SetFillableProperty.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dex\Laravel\Studio\Listeners;
+
+use Dex\Laravel\Studio\Blueprint\Blueprint;
+use Dex\Laravel\Studio\Blueprint\Draft;
+use Dex\Laravel\Studio\Blueprint\Preset;
+use Dex\Laravel\Studio\Generators\Generator;
+
+class SetFillableProperty
+{
+    public function __invoke(Generator $generator, Draft $draft, Blueprint $blueprint, Preset $preset): void
+    {
+        /** @var array $attributes */
+        $attributes = $draft->get('attributes', []);
+
+        $fillable = array_filter($attributes, fn ($attribute) => $attribute['fillable'] ?? false);
+
+        if ($fillable) {
+            $generator->class()
+                ->addProperty('fillable')
+                ->setProtected()
+                ->setValue(array_keys($fillable));
+        }
+    }
+}
