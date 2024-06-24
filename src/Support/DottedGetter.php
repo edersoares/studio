ae9dfa5579684b6e529e33f28d82588e5ca46e39
Eder Soares <edersoares@me.com>
2024-06-24 17:13:29 -0300

    Adds `settedAll`

--- src/Support/DottedGetter.php
+++ src/Support/DottedGetter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Dex\Laravel\Studio\Support;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 
 /**
@@ -19,5 +20,14 @@ trait DottedGetter
     public function setted(string $key, mixed $default = null): void
     {
         data_set($this->items, $key, $default);
+    }
+
+    public function settedAll(array $data): void
+    {
+        $dot = Arr::dot($data);
+
+        foreach ($dot as $key => $value) {
+            $this->setted($key, $value);
+        }
     }
 }
