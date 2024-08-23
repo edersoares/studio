870ce95a987e60c12bcc52cf16c11c4cb0c3f996
Eder Soares <edersoares@me.com>
2024-08-23 10:23:47 -0300

    Cleanup

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace Workbench\App\Providers;
 
-use Dex\Laravel\Studio\Blueprint\Blueprint;
-use Dex\Laravel\Studio\Blueprint\Draft;
-use Dex\Laravel\Studio\Blueprint\Preset;
-use Dex\Laravel\Studio\Generators\PhpGenerator;
-use Dex\Laravel\Studio\Generators\PhpGeneratorFactory;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class WorkbenchServiceProvider extends ServiceProvider
@@ -27,12 +21,6 @@ class WorkbenchServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Event::listen('generate:models', function (PhpGenerator $generator, Draft $draft, Blueprint $blueprint, Preset $preset) {
-            $generator->body(json_encode($blueprint->get('drafts')));
-        });
-
-        Event::listen('blueprint:draft', function (Draft $draft, Blueprint $blueprint, Preset $preset) {
-            PhpGeneratorFactory::new($draft, $blueprint, $preset);
-        });
+        //
     }
 }
