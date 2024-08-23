f907dd4c3bb9b5d130cc87f016b57a1abe1d44e3
Eder Soares <edersoares@me.com>
2024-08-23 13:48:21 -0300

    New draft

--- workbench/resources/studio.php
+++ workbench/resources/studio.php
@@ -13,13 +13,16 @@ return [
         Draft::new('User')
             ->set('table', 'user')
             ->attribute()->id()
-            ->attribute()->string('name')->fillable()->required()->min(3)->max(5)->faker('name')
+            ->attribute()->string('name')->fillable()->required()->min(3)->max(50)->faker('name')
+            ->attribute()->string('email')->unique()->fillable()->required()->min(3)->faker('email')
+            ->attribute()->string('password')->fillable()->required()->min(8)->max(48)->faker('lexify', '########')
             ->attribute()->timestamps()
             ->draft()
             ->push('generate', 'model')
             ->push('generate', 'migration:create')
             ->push('generate', 'factory')
             ->push('generate', 'controller')
+            ->push('generate', 'request')
             ->push('generate', 'tester')
             ->data(),
 
