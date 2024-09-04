53707911fc2fcdf8cf2c427ef59671bd8d048b2e
Eder Soares <edersoares@me.com>
2024-09-04 13:52:30 -0300

    Documentation

--- src/Modifier/Model/SetDocumentation.php
+++ src/Modifier/Model/SetDocumentation.php
@@ -6,6 +6,7 @@ namespace Dex\Laravel\Studio\Modifier\Model;
 
 use DateTime;
 use Dex\Laravel\Studio\Art;
+use Illuminate\Database\Eloquent\Collection;
 
 class SetDocumentation
 {
@@ -18,12 +19,20 @@ class SetDocumentation
             $name = $attribute['name'];
             $label = $attribute['label'] ?? '';
 
-            if ($type === 'id') {
+            if ($type === 'boolean') {
+                $type = 'bool';
+            }
+
+            if (in_array($type, ['integer', 'id', 'foreignId'], true)) {
                 $type = 'int';
             }
 
-            if (in_array($type, ['uuid', 'text', 'longText'], true)) {
+            if (in_array($type, ['uuid', 'text', 'longText', 'rememberToken'], true)) {
                 $type = 'string';
+            }
+
+            if ($type === 'json') {
+                $type = 'array';
             }
 
             if ($type === 'timestamps') {
@@ -32,18 +41,31 @@ class SetDocumentation
 
                 $art->generator()
                     ->class()
-                    ->addComment('@var DateTime $created_at')
-                    ->addComment('@var DateTime $updated_at');
+                    ->addComment('@property DateTime $created_at')
+                    ->addComment('@property DateTime $updated_at');
 
                 continue;
             }
 
+            if (in_array($type, ['timestamp', 'softDeletes'], true)) {
+                $type = 'DateTime';
+
+                $art->generator()
+                    ->namespace()->addUse(DateTime::class);
+            }
+
             $art->generator()
                 ->class()
-                ->addComment("@var $type \${$name} $label");
+                ->addComment("@property $type \${$name} $label");
         }
 
         $relations = $art->draft()->relations();
+
+        if ($relations) {
+            $art->generator()
+                ->class()
+                ->addComment('');
+        }
 
         foreach ($relations as $relation) {
             $type = $relation['type'];
@@ -61,7 +83,24 @@ class SetDocumentation
 
                 $art->generator()
                     ->class()
-                    ->addComment("@var $type \${$name} $label");
+                    ->addComment("@property $type \${$name} $label");
+            }
+
+            if ($type === 'hasMany') {
+                $type = $relation['model'];
+                $name = $relation['name'];
+                $label = $relation['label'] ?? '';
+
+                $class = $art->preset()->getNamespacedFor('model', $name);
+
+                $art->generator()
+                    ->namespace()
+                    ->addUse($class)
+                    ->addUse(Collection::class);
+
+                $art->generator()
+                    ->class()
+                    ->addComment("@property Collection<int, $type> \${$name} $label");
             }
         }
     }
