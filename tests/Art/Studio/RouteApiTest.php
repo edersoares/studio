f245de6028fc84a5fd808aed58a49ea820194341
Eder Soares <edersoares@me.com>
2024-10-22 20:00:54 -0300

    Fix `route:api`

--- tests/Art/Studio/RouteApiTest.php
+++ tests/Art/Studio/RouteApiTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Dex\Laravel\Studio\Draft;
 
 test('generate a api.php route file')
-    ->expect(fn () => Draft::new('User')->art('route:api', 'studio'))
+    ->expect(fn () => Draft::new('User')->set('endpoint', 'user')->set('reuse', false)->art('route:api', 'studio'))
     ->filename()
     ->toEndWith('routes/api.php')
     ->generate()
