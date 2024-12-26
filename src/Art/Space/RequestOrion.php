db9fb2f57cf4ea5857017176b048a895e14b75f3
Eder Soares <edersoares@me.com>
2024-12-26 19:48:17 -0300

    Missing class

---
+++ src/Art/Space/RequestOrion.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dex\Laravel\Studio\Art\Space;
+
+use Dex\Laravel\Studio\Art;
+use Dex\Laravel\Studio\Generators\GeneratePhp;
+use Dex\Laravel\Studio\Modifier\ClassNameFromPreset;
+use Dex\Laravel\Studio\Modifier\ExtendsFromPreset;
+use Dex\Laravel\Studio\Modifier\NamespaceFromPreset;
+use Dex\Laravel\Studio\Modifier\SetStrictTypesFromPreset;
+
+class RequestOrion extends Art
+{
+    use GeneratePhp;
+
+    public function apply(): array
+    {
+        return [
+            SetStrictTypesFromPreset::class,
+            NamespaceFromPreset::class,
+            ClassNameFromPreset::class,
+            ExtendsFromPreset::class,
+        ];
+    }
+
+    public function modify(Art $art): void
+    {
+        $rules = collect($art->draft()->attributes())
+            ->filter(fn ($attribute) => $attribute['validation']['rules'] ?? false)
+            ->map(fn ($attribute) => $attribute['validation']['rules'])
+            ->toArray();
+
+        $art->generator()->method('commonRules')
+            ->setReturnType('array')
+            ->setBody('return ?;', [$rules]);
+    }
+}
